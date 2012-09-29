2d3a6ed63036455847937cf00bec56b59ab36a9d
Raynos <raynos2@gmail.com>
2012-09-28 23:29:08 -0700

    first

---
+++ README.md
@@ -0,0 +1,25 @@
+# for-each
+
+A better forEach.
+
+## Example
+
+Like `Array.prototype.forEach` but works on objects
+
+```
+var forEach = require("for-each")
+
+forEach({ key: "value" }, function (value, key) {
+    /* code */
+})
+```
+
+## Installation
+
+`npm install for-each`
+
+## Contributors
+
+ - Raynos
+
+## MIT Licenced
---
+++ test/test.js
@@ -0,0 +1,27 @@
+var test = require("tap").test
+    , forEach = require("..")
+
+test("forEach calls each iterator", function (t) {
+    var count = 0
+    t.plan(4)
+    forEach({ a: 1, b: 2 }, function (value, key) {
+        if (count === 0) {
+            t.equal(value, 1)
+            t.equal(key, "a")
+        } else {
+            t.equal(value, 2)
+            t.equal(key, "b")
+        }
+        count++
+    })
+})
+
+test("forEach calls iterator with correct this value", function (t) {
+    var thisValue = {}
+
+    t.plan(1)
+
+    forEach([0], function () {
+        t.equal(this, thisValue)
+    }, thisValue)
+})
