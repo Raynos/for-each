d36f794d6c0c5696bf1e4f8e79ae667858dfc11b
Jordan Harband <ljharb@gmail.com>
2013-05-10 22:42:55 -0700

    Adding proper array indexing, as well as string support.

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 module.exports = forEach
 
 function forEach(list, iterator, context) {
-    var keys = Object.keys(list)
+    var isArray = Object.prototype.toString.call(list) === '[object Array]'
+        , isString = typeof list === 'string'
+        , keys = isString ? list : Object.keys(list)
 
     if (arguments.length < 3) {
         context = this
     }
 
     for (var i = 0, len = keys.length; i < len; i++) {
-        var key = keys[i]
-            , value = list[key]
+        var key = isString || isArray ? i : keys[i]
+            , value = isString ? keys.charAt(i) : list[key]
 
         iterator.call(context, value, key, list)
     }
 }
+
